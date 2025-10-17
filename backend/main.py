1c3301dc565122e1b055448eab695f45a4d2c650
cku <xidanhgiaychuan@gmail.com>
2025-10-17 23:16:10 +0700

    rmcahce

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,9 @@ import os
 
 app = modal.App("music-generator")
 
-# Use an official Python slim image that provides Python >= 3.9 so
-# spaCy 3.8.4 (required by ACE-Step) can be installed.
 image = (
     modal.Image.debian_slim()
     .apt_install("git")
-    # Make sure pip/setuptools/wheel are up-to-date before building ACE-Step
     .run_commands(
         "git clone https://github.com/ace-step/ACE-Step.git /tmp/ACE-Step",
         "cd /tmp/ACE-Step && python -m pip install .",
@@ -23,10 +20,52 @@ hf_volume = modal.Volume.from_name("qwen-hf-cache", create_if_missing=True)
 music_gen_secrets = modal.Secret.from_name("music-gen-secret")
 
 
-@app.function(image=image, secrets=[music_gen_secrets])
-def function_test():
-    print("hello")
-    print(os.environ["just_a_test"])
+# @app.function(image=image, secrets=[music_gen_secrets])
+# def function_test():
+#     print("hello")
+#     print(os.environ["just_a_test"])
+
+
+@app.cls(
+    image=image,
+    gpu="L40S",
+    volumes={"/models": model_volume, "/.cache/huggingface": hf_volume},
+    secrets=[music_gen_secrets],
+    scaledown_window=15,
+)
+class MusicGenServer:
+    @modal.enter()
+    def load_model(self):
+        from acestep.pipeline_ace_step import ACEStepPipeline
+        from transformers import AutoProcessor, Qwen3VLMoeForConditionalGeneration
+        from diffusers import AutoPipelineForText2Image
+        import torch
+
+        # Music Generation model
+
+        self.music_model = ACEStepPipeline(
+            checkpoint_dir="/models",
+            dtype="bfloat16",
+            torch_compile=False,
+            cpu_offload=False,
+            overlapped_decode=False,
+        )
+
+        # Large Language Model
+        model_id = "Qwen/Qwen3-VL-235B-A22B-Instruct"
+        self.processor = AutoProcessor.from_pretrained(model_id)
+        self.llm_model = Qwen3VLMoeForConditionalGeneration.from_pretrained(
+            model_id, dtype="auto", device_map="auto", cache_dir="/.cache/huggingface"
+        )
+
+        def test_endpoint(self):
+            self.music_model
+
+        # Stable Difusion Model (thumbnail)
+        pipe = AutoPipelineForText2Image.from_pretrained(
+            "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16"
+        )
+        pipe.to("cuda")
 
 
 @app.local_entrypoint()
